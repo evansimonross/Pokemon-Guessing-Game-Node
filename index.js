00eb7beae9e3669d7911277c4201d8757c7fa57a
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-08-24 00:58:25 -0400

    Finished app structure + prompt behavior

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 var request = require("request");
 var prompt = require("prompt");
+var colors = require("colors/safe");
 var Word = require("./word.js");
 
 var guessSchema = {
   properties: {
     guess: {
       pattern: /^[a-zA-Z]{1}$/,
+      description: colors.yellow("Make a Guess"),
       message: 'Please guess a letter!',
       required: true
     },
   }
 };
 
+prompt.message = "";
+prompt.delimiter = colors.yellow(":");
+
 var promptGuess = (word) => {
   console.log(word + "");
   prompt.start();
   prompt.get(guessSchema, function (err, result) {
+    if (err) {
+      console.log(colors.red("Something went wrong"));
+      process.exit();
+    }
     var guessedLetter = result.guess;
     if (word.guess(guessedLetter)) {
-      console.log("CORRECT!");
+      console.log(colors.cyan("CORRECT!"));
     }
     else {
-      console.log("TRY AGAIN");
+      console.log(colors.magenta("TRY AGAIN"));
     }
     if (word.remaining === 0) {
-      console.log("CONGRATULATIONS");
+      console.log(colors.rainbow("CONGRATULATIONS"));
+      console.log(colors.cyan(word + ""));
+      start(false);
     }
     else {
       promptGuess(word);
@@ -33,14 +44,51 @@ var promptGuess = (word) => {
 }
 
 var newGame = () => {
-  console.log("Welcome to the Pokemon Guessing Game 2.0");
+  console.log(colors.magenta("A Pokemon is being chosen..."));
   var number = Math.floor(Math.random() * 807);
   request("https://pokeapi.co/api/v2/pokemon/" + number, function (err, res, body) {
     if (!err && res.statusCode === 200) {
       let pokemon = JSON.parse(body).name;
       promptGuess(new Word(pokemon));
     }
+    else {
+      console.log(colors.red("Something went wrong"));
+      process.exit();
+    }
   });
 }
 
-newGame();
+var start = (first) => {
+  if (first) {
+    console.log(colors.rainbow("Welcome to the Pokemon Guessing Game 2.0"));
+    newGame();
+  }
+  else {
+    prompt.start();
+    prompt.get({
+      properties: {
+        newGame: {
+          pattern: /Y|N|y|n{1}/,
+          description: colors.yellow("Start a new game (Y/N)"),
+          message: 'Please choose Y/N',
+          required: true
+        }
+      }
+    }, function (err, result) {
+      if (err) {
+        console.log(colors.red("Something went wrong"));
+        process.exit();
+      }
+      var choice = result.newGame.toUpperCase();
+      if (choice === "Y") {
+        newGame();
+      }
+      else {
+        console.log(colors.green("Thanks for playing!"));
+        process.exit();
+      }
+    });
+  }
+}
+
+start(true);
--- word.js
+++ word.js
@@ -1,28 +1,28 @@
 var Letter = require("./letter.js");
 
-Word = function(word){
+Word = function (word) {
     this.letters = word.split("");
     this.remaining = this.letters.length;
-    for (i=0; i<this.letters.length; i++){
+    for (i = 0; i < this.letters.length; i++) {
         this.letters[i] = new Letter(this.letters[i]);
-        if(this.letters[i].char===" ") {
-            this.remaining --;
+        if (this.letters[i].char === " ") {
+            this.remaining = - 1;
         }
     }
 }
 
-Word.prototype.guess = function(guessedLetter){
+Word.prototype.guess = function (guessedLetter) {
     var correct = false;
-    this.letters.forEach(function(letter){
-        if(letter.guess(guessedLetter)){
+    this.letters.forEach((letter) => {
+        if (letter.guess(guessedLetter)) {
             correct = true;
-            this.remaining --;
+            this.remaining -= 1;
         }
     });
     return correct;
 }
 
-Word.prototype.toString = function (){
+Word.prototype.toString = function () {
     return this.letters.join(" ");
 }
 
